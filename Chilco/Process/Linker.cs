2531dbf47b8bb3488e451f1be20c2802d6f4733a
Witteborn <Witteborn@posteo.de>
2019-11-22 00:57:56 -0800

    Removed Butcher object from Linker

--- Chilco/Process/Linker.cs
+++ Chilco/Process/Linker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +11,10 @@ namespace Chilco
     {
         public Tracker Tracker { get; private set; }
         public Group Group { get; private set; }
-        public Butcher Butcher { get; private set; }
 
-        public Linker(Tracker tracker, Group group, Butcher butcher) {
+        public Linker(Tracker tracker, Group group) {
             this.Tracker = tracker;
             this.Group = group;
-            this.Butcher = butcher;
         }
 
 
