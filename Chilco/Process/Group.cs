3225f4fe1b646b8b4a0a6bb6a757f719fe13c29a
Witteborn <Witteborn@posteo.de>
2019-11-21 22:55:31 -0800

    rearranged classes

---
+++ Chilco/Process/Group.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chilco
+{
+    internal class Group
+    {
+        public TimeSpan LeftoverTime;
+        public DateTime DateLastRun;
+        public Ruleset ruleset;
+
+        public Group(string title, List<String> processes, TimeSpan leftoverTime, bool doTimeRollover, DateTime dateLastRun, TimeSpan dailyPlaytime)
+        {
+            this.LeftoverTime = leftoverTime;
+            this.DateLastRun = dateLastRun;
+            this.ruleset = new Ruleset(title, processes, doTimeRollover, dailyPlaytime);
+            Settings.GetInstance().settings.Add(this);
+        }
+
+
+        public struct Ruleset {
+            public string Title;
+            public List<string> Processes;
+            public bool DoTimeRollover;
+            public TimeSpan DailyPlaytime;
+            public Ruleset(string Title, List<string> Processes, bool DoTimeRollover, TimeSpan DailyPlaytime)
+            {
+                this.Title = Title;
+                this.Processes = Processes;
+                this.DoTimeRollover = DoTimeRollover;
+                this.DailyPlaytime = DailyPlaytime;
+            }
+        }
+
+    }
+}
