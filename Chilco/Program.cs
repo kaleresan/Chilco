f2cbb60f119e157a38376dc2596f8635e49c762b
Witteborn <Witteborn@posteo.de>
2019-11-21 23:32:47 -0800

    Implemented method which writes application into startup

--- Chilco/Program.cs
+++ Chilco/Program.cs
@@ -1,17 +1,34 @@
 using System;
-using System.Collections.Generic;
+using System.IO;
 
 namespace Chilco
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
+        }
 
+        public static void ImplementAutostart()
+        {
+            string startup = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartup);
+            string application = System.Reflection.Assembly.GetEntryAssembly().Location;
+            string applicationName = Path.GetFileName(application);
+
+            string startup_applicationName = Path.Combine(startup, applicationName);
+
+            try
+            {
+                File.Move(application, startup_applicationName);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
         }
     }
 }
