058a65410cf72a87ac19344987327637d568c670
kodeeey <kodeeey@gmail.com>
2019-11-26 13:30:37 +0100

    -Reformating code

--- web-app/src/components/Main.js
+++ web-app/src/components/Main.js
@@ -1,41 +1,39 @@
 import React, { Component } from "react";
-import MainAppBar from "./MainAppBar"
-import SideDrawer from "./SideDrawer"
-import Devices from "./Devices"
-import DeviceSettings from "./DeviceSettings"
+import MainAppBar from "./MainAppBar";
+import SideDrawer from "./SideDrawer";
+import Devices from "./Devices";
+import DeviceSettings from "./DeviceSettings";
 
 export class Main extends Component {
-  constructor (props) {
-   super(props)
-   this.state = {
-     openMenuBar: false
-   }
-   this.handleMenuBarOpen = this.handleMenuBarOpen.bind(this);
-   this.handleMenuBarClose = this.handleMenuBarClose.bind(this);
- }
+  constructor(props) {
+    super(props);
+    this.state = {
+      openMenuBar: false
+    };
+    this.handleMenuBarOpen = this.handleMenuBarOpen.bind(this);
+    this.handleMenuBarClose = this.handleMenuBarClose.bind(this);
+  }
 
- handleMenuBarOpen() {
-   this.setState({openMenuBar: true})
- }
+  handleMenuBarOpen() {
+    this.setState({ openMenuBar: true });
+  }
 
- handleMenuBarClose() {
-   this.setState({openMenuBar: false})
- }
+  handleMenuBarClose() {
+    this.setState({ openMenuBar: false });
+  }
 
   render() {
-  return (
-    <div id="parent">
-    <MainAppBar
-    onChildClick={this.handleMenuBarOpen}
-    />
-    <DeviceSettings/>
-    <SideDrawer
-    openMenuBar={this.state.openMenuBar} onMenuBarClose={this.handleMenuBarClose}
-    />
-
-    </div>
-  )
-}
+    return (
+      <div id="parent">
+        <MainAppBar onChildClick={this.handleMenuBarOpen} />
+        <DeviceSettings />
+        <SideDrawer
+          openMenuBar={this.state.openMenuBar}
+          onMenuBarClose={this.handleMenuBarClose}
+        />
+      </div>
+    );
+  }
 }
 
 export default Main;
