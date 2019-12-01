dedfda628a534065f174b8ddff5559b5d6fd550d
Pavel Keßler <pavel.kessler@gmx.de>
2019-12-01 17:01:07 +0100

    Dia und Code abgeglichen

--- Chilco/ConnectionHandler.cs
+++ Chilco/ConnectionHandler.cs
@@ -111,7 +111,7 @@ namespace Chilco
                 }
             }
 
-            Main.Update(RulesetList.ToArray());
+            Main.Update(RulesetList);
         }
 
         private static Ruleset[] GetDefaultRulesets()
