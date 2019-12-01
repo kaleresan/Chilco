e29049c61936b12d587b83f198a65d39a0922058
Pavel Keßler <pavel.kessler@gmx.de>
2019-12-01 22:19:20 +0100

    Fix: Butcher really doesn't rampage anymore

--- Chilco/Process/Butcher.cs
+++ Chilco/Process/Butcher.cs
@@ -18,11 +18,18 @@ namespace Chilco
         {
             System.Console.WriteLine("Trying to kill process: "+ processName);
             //For every Process currently running that has the name s
-            foreach (Process p in Process.GetProcesses().Where(p2 => p2.ProcessName == processName || (p2.MainWindowTitle == processName && p2.MainWindowTitle != "")))
+            foreach (Process p in Process.GetProcesses().Where(
+                p2 => p2.ProcessName == processName || 
+                (p2.MainWindowTitle == processName && 
+                p2.MainWindowTitle != "" && 
+                processName != "")))
             {
                 //Checks if process is actually running
                 //(sometimes the Process closes itself before being killed and Chilco crashes)
-                if (Process.GetProcesses().Count(p3 => p3.ProcessName == processName || (p2.MainWindowTitle == processName && p2.MainWindowTitle != "")) > 0)
+                if (Process.GetProcesses().Count(p3 => p3.ProcessName == processName ||
+                    (p3.MainWindowTitle == processName && 
+                    p3.MainWindowTitle != "" && 
+                    processName != "")) > 0)
                 {
                     System.Console.WriteLine("Killing process: " + processName);
                     p.Kill();
